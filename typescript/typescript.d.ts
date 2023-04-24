e731029a7652912faff6c3e9280e42c66c85ad7a
ToyCon <toystosound@gmail.com>
2023-04-24 22:27:30 +0900

    Update Nextjs project1

--- typescript/typescript.d.ts
+++
@@ -1,5 +0,0 @@
-declare let 학교: {
-    score: (number | boolean)[];
-    teacher: string;
-    friend: string | string[];
-};
--- typescript/typescript.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let 학교 = {
-    score: [100, 97, 84],
-    teacher: 'Phil',
-    friend: 'John'
-};
-학교.score[4] = false;
-학교.friend = ['Lee', 학교.teacher];
